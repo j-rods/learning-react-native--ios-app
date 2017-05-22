b73ee789acbfb6d473cdad697abc062757b26384
Jessica Rodriguez <jrrodriguez84@gmail.com>
2017-05-22 13:22:09 +0100

    write the function to be rendered and write import statements

--- index.ios.js
+++ index.ios.js
@@ -1,9 +1,20 @@
+// Import some code we need
+import React, { component } from 'react';
+import { AppRegistry, Text, View } from 'react-native';
+
 // Create a react component
 var Weekdays = React.createClass({
   render: function() {
-
+    return <View>
+      <Text>
+        Days of the week:
+      </Text>
+    </View>
   }
 });
 
 
 // Show the react component on the screen
+AppRegistry.registerComponent('weekdays', function() {
+  return Weekdays
+});
