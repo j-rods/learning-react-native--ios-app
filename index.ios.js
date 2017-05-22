0dfbc9903e07650293c651afedbea85f4750ed43
Jessica Rodriguez <jrrodriguez84@gmail.com>
2017-05-22 15:53:28 +0100

    use moment js to generate the days array and loop over to display starting from current day

--- index.ios.js
+++ index.ios.js
@@ -1,26 +1,27 @@
 // Import some code we need
+import Moment from 'moment';
 import React, { component } from 'react';
 import { AppRegistry, Text, View, StyleSheet } from 'react-native';
 import DayItem from './src/day-item';
 
-var DAYS = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 
 
 // Create a react component
 var Weekdays = React.createClass({
   render: function() {
     return <View style={styles.container}>
-      <Text>
-        Days of the week:
-      </Text>
       {this.days()}
     </View>
   },
   days: function() {
-    return DAYS.map(function(day) {
-      // Called 7 times, one for each day of the week
-      return <DayItem day={day} />
-    });
+    var daysItems = [];
+    for(var i = 0; i < 7; i++) {
+      var day = Moment().add(i, 'days').format('dddd');
+      daysItems.push(
+        <DayItem day={day} daysUntil={i} />
+      )
+    }
+    return daysItems
   }
 });
 
